810acf9ea5d2c96014c29adc5fd8761beecca68a
VARUN <arunkumarramavath23@gmail.com>
2025-11-01 10:33:58 +0530

    New commit

--- app.py
+++ app.py
@@ -330,4 +330,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         init_sample_data()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
